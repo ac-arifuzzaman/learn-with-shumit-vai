1454e564dc9d4c8e3acbac9b15f73008ba3ce86f
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-02-25 11:34:55 +0600

    add middlewere function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import DynamicHooksCounter from "./components/DynamicHooksCounter";
 import HooksCounter from "./components/HooksCounter";
+import VariableCounter from "./components/VariableCounter";
 import store from "./redux/counter/store";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <div className="max-w-md mx-auto mt-10 space-y-5">
           <HooksCounter />
           <DynamicHooksCounter />
+          <VariableCounter dynamic />
         </div>
       </div>
     </Provider>
--- src/redux/counter/store.js
+++ src/redux/counter/store.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../rootReducer";
 
-const store = createStore(rootReducer);
+const myLoger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Befor: ${JSON.stringify(store.getState())}`);
+  const upcommingState = [action].reduce(rootReducer, store.getState());
+  console.log(JSON.stringify(upcommingState));
+  return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLoger));
 
 export default store;
