2aa5d6e913b4830079b9e85a017371106a9580dd
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-02-26 11:53:21 +0600

    add basic components and implement it

--- src/redux/counter/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
-import myLoger from "../middlewers/myLoger";
-import rootReducer from "../rootReducer";
-
-const store = createStore(rootReducer, applyMiddleware(myLoger, logger));
-
-export default store;
