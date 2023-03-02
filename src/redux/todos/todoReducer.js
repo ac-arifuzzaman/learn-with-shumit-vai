661fff7a2007cf3098ebe4141995c3031727216d
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-03-02 12:22:56 +0600

    add todo reducer case using action types

---
+++ src/redux/todos/todoReducer.js
@@ -0,0 +1,67 @@
+import {
+    ADDED,
+    ALLCOMPLETED,
+    CLEARCOMPLETED,
+    COLORSELECTED,
+    DELETED,
+    TOOGLED
+} from "./actionTypes";
+import { initialState } from "./initialState";
+
+const nextId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(maxId, todo.id), -1);
+  return maxId + 1;
+};
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: nextId(state),
+        },
+      ];
+
+    case TOOGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+
+    case COLORSELECTED:
+      const { todoId, color } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: color,
+        };
+      });
+
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case ALLCOMPLETED:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+
+    case CLEARCOMPLETED:
+      return state.map((todo) => !todo.completed);
+
+    default:
+      break;
+  }
+};
+
+export default todoReducer;
