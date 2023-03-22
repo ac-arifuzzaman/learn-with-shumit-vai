f9cce9946a543388fa34d91cc0c17e84e6a34a07
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-03-22 16:21:51 +0600

    add video code using useparams

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tagReducer from "../features/tags/tagsSlice";
+import videoReducer from "../features/video/videoSlice";
 import videosReducer from "../features/videos/videosSlice";
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagReducer,
+    video: videoReducer,
   },
 });
