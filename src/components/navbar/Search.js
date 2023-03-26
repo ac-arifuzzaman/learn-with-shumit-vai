88d684c4b15c512deb65dee28585e18dc3065cc3
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2023-03-26 12:16:54 +0600

    add filterring option using querystring

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useMatch, useNavigate } from "react-router-dom";
 import { searched } from "../../features/filter/filterSlice";
 
 export default function Search() {
   const dispatch = useDispatch();
   const { search } = useSelector((state) => state.filter);
+  const match = useMatch("/");
+  const navigate = useNavigate();
 
   const [input, setInput] = useState(search);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(searched(input));
+
+    if (!match) {
+      navigate("/");
+    }
     setInput("");
   };
 
